d13263bb8def13267029be19bf01547ac39b5341
Plain-ST <Plain.11.dc@gmail.com>
2021-05-11 11:21:30 +0900

    ファイルの読み書きテスト

--- controller_test.py
+++ controller_test.py
@@ -1,4 +1,17 @@
 #ファイル読み込みテスト
-myfile = open('text_test.txt')
-lines =myfile.read()
-print(lines)
+filename = 'text_test.txt'
+with open(filename,'r') as f:
+  filedata=f.read()
+filelist=filedata.split('\n')
+print(filelist)
+if filelist[0]=='text: ':
+  intext = input('textを入力してください：')
+  filelist[0]='text: '+intext+'\n'
+if filelist[1]=='number: ':
+  innumber = input('numberを入力してください：')
+  filelist[1]='number: '+innumber
+print(filelist)
+
+with open(filename,'w') as f:
+  for i in filelist:
+    f.write(i)
